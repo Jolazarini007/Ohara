1b54e598d4bcbb84572c2b2cfae3ebf4f90c7b52
juan <juan.ferreira12@etec.sp.gov.br>
2024-11-21 17:10:24 -0300

    feat: mais mudancas minimas no css

--- resources/views/professor/listPresenca.blade.php
+++ resources/views/professor/listPresenca.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('css/professores/menuLateral.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/menuLateral.css') }}">
     <link rel="stylesheet" href="{{ asset('css/professores/listPresenca.css') }}">
     <title>Ohara</title>
 </head>
